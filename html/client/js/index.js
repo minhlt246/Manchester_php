52f8bc2414f4e9e375974823cc5257bac4fabcce
David <150817765+minhlt246@users.noreply.github.com>
2024-12-26 16:35:30 +0700

    push

---
+++ html/client/js/index.js
@@ -0,0 +1,31 @@
+$(".vegetable-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 2000,
+    center: false,
+    dots: true,
+    loop: true,
+    margin: 25,
+    nav : true,
+    navText : [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:1
+        },
+        992:{
+            items:2
+        },
+        1200:{
+            items:2
+        }
+    }
+});
